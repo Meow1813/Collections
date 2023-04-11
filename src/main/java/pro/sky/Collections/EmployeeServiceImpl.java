18da54050cb6f8469a4523ba4240a791209161be
Alex <shurik228154@mail.ru>
2023-04-11 18:18:22 +0300

    Сделанное ДЗ

--- src/main/java/pro/sky/Collections/EmployeeServiceImpl.java
+++ src/main/java/pro/sky/Collections/EmployeeServiceImpl.java
@@ -4,28 +4,31 @@ import org.springframework.stereotype.Service;
 import pro.sky.Collections.Exception.EmployeeAlreadyAddedException;
 import pro.sky.Collections.Exception.EmployeeNotFoundException;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
-    public static List<Employee> employees = new ArrayList<>();
+    public static Map<String, Employee> employees = new HashMap<>();
+    public static String getKey(Employee employee){
+        return employee.getFirstName() + " " + employee.getLastName();
+    }
 
     @Override
     public String addEmployee(String firstName, String lastName) { //добавление сотрудника
         Employee newEmployee = new Employee(firstName, lastName);
-        if (employees.contains(newEmployee)) {
+        if (employees.containsKey(getKey(newEmployee))) {
             throw new EmployeeAlreadyAddedException();
         }
-        employees.add(newEmployee);
+        employees.put(getKey(newEmployee),newEmployee);
         return newEmployee.toString();
     }
 
     @Override
     public String deleteEmployee(String firstName, String lastName) { //удаление сотрудника
         Employee employee = new Employee(firstName, lastName);
-        if (employees.contains(employee)) {
-            employees.remove(employee);
+        if (employees.containsKey(getKey(employee))) {
+            employees.remove(getKey(employee));
             return employee.toString();
         }
         throw new EmployeeNotFoundException();
@@ -34,7 +37,7 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public String findEmployee(String firstName, String lastName) {
         Employee employee = new Employee(firstName, lastName);
-        if (employees.contains(employee)) {
+        if (employees.containsKey(getKey(employee))) {
             return employee.toString();
         }
         throw new EmployeeNotFoundException();
